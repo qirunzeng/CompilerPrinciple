cda5b14412adeab4845adead6572f17ed9a3073c
Qirun Zeng <rewinery@outlook.com>
2024-12-15 20:19:32 +0800

    优化文件结构

---
+++ main.c
@@ -0,0 +1,61 @@
+#include "pl0.c"
+
+int main ()
+{
+	FILE* hbin;
+	char s[80];
+	int i;
+	symset set, set1, set2;
+
+	printf("Please input source file name: "); // get file name to be compiled
+	scanf("%s", s);
+	if ((infile = fopen(s, "r")) == NULL)
+	{
+		printf("File %s can't be opened.\n", s);
+		exit(1);
+	}
+
+	phi = createset(SYM_NULL);
+	relset = createset(SYM_EQU, SYM_NEQ, SYM_LES, SYM_LEQ, SYM_GTR, SYM_GEQ, SYM_NULL);
+	
+	// create begin symbol sets
+	declbegsys = createset(SYM_CONST, SYM_VAR, SYM_PROCEDURE, SYM_NULL);
+	statbegsys = createset(SYM_BEGIN, SYM_CALL, SYM_IF, SYM_WHILE, SYM_NULL);
+	facbegsys = createset(SYM_IDENTIFIER, SYM_NUMBER, SYM_LPAREN, SYM_MINUS, SYM_NULL);
+
+	err = char_cnt = curr_ins = line_length = 0; // initialize global variables
+	ch = ' ';
+	kk = MAXIDLEN;
+
+	getsym();
+
+	set1 = createset(SYM_PERIOD, SYM_NULL);
+	set2 = uniteset(declbegsys, statbegsys);
+	set = uniteset(set1, set2);
+	block(set);
+	destroyset(set1);
+	destroyset(set2);
+	destroyset(set);
+	destroyset(phi);
+	destroyset(relset);
+	destroyset(declbegsys);
+	destroyset(statbegsys);
+	destroyset(facbegsys);
+
+	if (sym != SYM_PERIOD)
+		error(9); // '.' expected.
+	if (err == 0)
+	{
+		hbin = fopen("hbin.txt", "w");
+		for (i = 0; i < curr_ins; i++)
+			fwrite(&code[i], sizeof(instruction), 1, hbin);
+		fclose(hbin);
+	}
+	if (err == 0)
+		interpret();
+	else
+		printf("There are %d error(s) in PL/0 program.\n", err);
+	listcode(0, curr_ins);
+    return 0;
+} // main
+
