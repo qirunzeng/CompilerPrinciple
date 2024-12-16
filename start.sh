e08054f167d309a72ef3bb331cac8698e0ff508b
Qirun Zeng <rewinery@outlook.com>
2024-12-16 17:36:32 +0800

    脚本

--- start.sh
+++ start.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Check if build directory exists, if not create it
 if [ ! -d "build" ]; then
     mkdir build
 fi
 
-# Navigate to build directory
 cd build
 
-# Run cmake and make
 cmake ..
 make
 
 echo "\n-------------------------\n\n"
-# Execute the example.txt file with pl0_compiler
 echo "../example.txt" | ./pl0_compiler
 
 echo "Done"
