696fe6d8d78e65986c86a4c2cf5810a59c5dc4c3
Qirun Zeng <rewinery@outlook.com>
2024-12-17 00:04:48 +0800

    实现了与或非

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include "pl0.h"
 
-int main () {
-	FILE* hbin;
-	char s[80] = "../example.txt";
+int main()
+{
+	FILE *hbin;
+	char s[80];
 	int i;
 	symset set, set1, set2;
 
@@ -15,18 +16,16 @@ int main () {
 	}
 
 	phi = createset(SYM_NULL);
-	relset = createset(SYM_EQU, SYM_NEQ, SYM_LES, SYM_LEQ, SYM_GTR, SYM_GEQ); // relational operators
-	
+
 	// create begin symbol sets
 	declbegsys = createset(SYM_CONST, SYM_VAR, SYM_PROCEDURE, SYM_NULL);
 	statbegsys = createset(SYM_BEGIN, SYM_CALL, SYM_IF, SYM_WHILE, SYM_NULL);
-	facbegsys = createset(SYM_IDENTIFIER, SYM_NUMBER, SYM_LPAREN, SYM_MINUS, SYM_NULL);
+	facbegsys = createset(SYM_IDENTIFIER, SYM_NUMBER, SYM_LPAREN, SYM_MINUS, SYM_NOT,SYM_NULL);
 
-	err = char_cnt = curr_ins = line_length = 0; // initialize global variables
+	err = char_cnt = curr_ins = line_length = 0;// initialize global variables
 	ch = ' ';
 	id_index = MAXIDLEN;
 
-    // printf("%d -- PL/0 compiler: %s\n", __LINE__,  s); fflush(stdout);
 	getsym();
 
 	set1 = createset(SYM_PERIOD, SYM_NULL);
@@ -37,19 +36,17 @@ int main () {
 	destroyset(set2);
 	destroyset(set);
 	destroyset(phi);
-	destroyset(relset);
 	destroyset(declbegsys);
 	destroyset(statbegsys);
 	destroyset(facbegsys);
 
-	if (sym != SYM_PERIOD) {
+	if (sym != SYM_PERIOD)
 		error(9); // '.' expected.
-    }
-	if (err == 0) {
+	if (err == 0)
+	{
 		hbin = fopen("hbin.txt", "w");
-		for (i = 0; i < curr_ins; i++) {
+		for (i = 0; i < curr_ins; i++)
 			fwrite(&code[i], sizeof(instruction), 1, hbin);
-        }
 		fclose(hbin);
 	}
 	if (err == 0)
@@ -57,6 +54,5 @@ int main () {
 	else
 		printf("There are %d error(s) in PL/0 program.\n", err);
 	listcode(0, curr_ins);
-    return 0;
+	return 0;
 } // main
-
